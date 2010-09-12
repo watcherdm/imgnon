f4ab0f4a59a956e2cc1f625db2aeb52980596ddb
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-09-12 16:05:39 -0700

    tweaking the settings looking for the sweey spot

--- contrast/views.py
+++ contrast/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from imgnon.contrast.models import Image as img
 import StringIO
-from PIL import Image, ImageEnhance
-# Create your views here.
+from PIL import Image, ImageEnhance, ImageOps
+from imgnon.contrast.sbm import stickybits
+import simplejson as json
+TEST_KEY = "c1e1928908a544dbc15b5e0231887a58"
+
 def index(request):
   return render_to_response('adjust.html')
 
@@ -21,21 +24,49 @@ def adjust(request):
     post = request.POST
     files = request.FILES
     image = request.FILES['img']
-    con = request.POST['amt']
-    imagen = Image.open(StringIO.StringIO(image.read()))
-    bri = ImageEnhance.Brightness(imagen)
-    imagen = bri.enhance(float(con))
-    enh = ImageEnhance.Contrast(imagen)
-    out = StringIO.StringIO()
-    imagen = enh.enhance(float(con))
-    imagen.save(out, "PNG")
-    cont = out.getvalue()
-    out.close()
-    return HttpResponse(cont, mimetype='image/png')
+    dest = open('/home/gabriel/imgnon/tmp/f.jpg', 'wb+')
+    for chunk in image.chunks():
+      dest.write(chunk)
+    dest.close()
+    con = 1.0
+    tries = 0
+    cont = upload_image('/home/gabriel/imgnon/tmp/f.jpg')
+    current = '/home/gabriel/imgnon/tmp/f.jpg'
+    while len(cont['codes']) == 0 and tries < 10:
+      con += 0.3
+      tries += 1
+      imagen = Image.open(current)
+      #bbox = (imagen.size[0] * ((con - 1)/10), imagen.size[1] * ((con - 1)/8), imagen.size[0] - imagen.size[0] * ((con - 1)/10), imagen.size[1] - imagen.size[1] * ((con - 1)/8))
+      #imagen = imagen.crop(bbox)
+      imagen = ImageOps.grayscale(imagen)
+      bri = ImageEnhance.Brightness(imagen)
+      imagen = bri.enhance(float(con) * 1.2)
+      enh = ImageEnhance.Contrast(imagen)
+      imagen = enh.enhance(float(con))
+      imagen.show()
+      current = '/home/gabriel/imgnon/tmp/f%d.jpg' % (con * 100)
+      imagen.save(current, "JPEG")
+      cont = upload_image(current)
+    return HttpResponse(json.dumps({'codes':cont['codes'],'tries':tries}))
   else:
     return Http404
-def handle_uploaded_file(f):
-  destination = open('/home/gabriel/tmp/imgnon/contrast.png', 'wb+')
-  for chunk in f.chunks():
-    destination.write(chunk)
-  destination.close()
+
+def upload_image(code_image, **kwargs):
+  sb = stickybits.Stickybits(apikey=TEST_KEY)
+  sb.base_url = 'http://dev.stickybits.com/api/2/'
+  data = {}
+  headers = None
+  data['code_image'] = code_image
+
+  try:
+    content_type, body = stickybits.file_encode(data['code_image'])
+  except:
+    return False
+
+  headers = {
+  'Content-Type': content_type
+  }
+  data = body
+
+  return sb.request('scan.create', kwargs, "POST",
+  data=data, headers=headers)
